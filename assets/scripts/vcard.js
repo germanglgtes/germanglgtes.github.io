7d4a63128fdc9b1b224361fcc5d9fb7ee046724c
germanglgtes <140262392+germanglgtes@users.noreply.github.com>
2023-07-24 14:51:13 +0200

    Update vcard.js

--- assets/scripts/vcard.js
+++ assets/scripts/vcard.js
@@ -1,17 +1,26 @@
 document.querySelector("input").addEventListener("click", function () {
-  var contact = {
-    name: "Germán Carlos López Gargallo",
-    email: "german@glgt.es",
-    web: "www.glgt.es"
-  };
-  // create a vcard file
-  var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nURL:" + contact.web + "\nEMAIL;TYPE=work:" + contact.email + "\nEND:VCARD";
+  var vcard = `
+BEGIN:VCARD
+VERSION:4.0
+FN:Germán Carlos López Gargallo
+URL:german@glgt.es
+ORG:GLG Traducciones
+EMAIL;TYPE=work:german@glgt.es
+GENDER:M
+KIND:individual
+LOGO;TYPE=PNG:http://www.glgt.es/assets/images/glgt_logo.png
+LANG;TYPE=work;PREF=1:en
+LANG;TYPE=work;PREF=2:pt
+LANG;TYPE=home:es
+NOTE:Traductor, subtitulador y redactor EN, PT > ES
+TZ:Europe/Madrid
+END:VCARD`;
   var blob = new Blob([vcard], { type: "text/vcard" });
   var url = URL.createObjectURL(blob);
   
   const newLink = document.createElement('a');
-  newLink.download = contact.name + ".vcf";
-  newLink.textContent = contact.name;
+  newLink.download = "Germán Carlos López Gargallo.vcf";
+  newLink.textContent = "Germán Carlos López Gargallo";
   newLink.href = url;
   
   newLink.click();
