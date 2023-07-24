d53abd7bbad74dacc1fee9c5658bbaf93714e040
germanglgtes <140262392+germanglgtes@users.noreply.github.com>
2023-07-24 14:21:09 +0200

    Create vcard.js

---
+++ assets/scripts/vcard.js
@@ -0,0 +1,18 @@
+document.querySelector("input").addEventListener("click", function () {
+  var contact = {
+    name: "Germán Carlos López Gargallo",
+    email: "german@glgt.es",
+    web: "www.glgt.es"
+  };
+  // create a vcard file
+  var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nURL:" + contact.web + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+  var blob = new Blob([vcard], { type: "text/vcard" });
+  var url = URL.createObjectURL(blob);
+  
+  const newLink = document.createElement('a');
+  newLink.download = contact.name + ".vcf";
+  newLink.textContent = contact.name;
+  newLink.href = url;
+  
+  newLink.click();
+});
